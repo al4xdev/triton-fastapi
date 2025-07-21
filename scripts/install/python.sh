d62f6459e1979d81ddc8871290d8ae2f7908b6e0
--alex_- <--alexsandro.pessoa@accenture.com>
2025-07-21 17:02:01 -0300

    Feat: emprestado

--- scripts/install/python.sh
+++ scripts/install/python.sh
@@ -1,5 +1,4 @@
-PROJECT_ROOT="$(dirname "$(dirname "$0")")"
-MIDDLEWARE_DIR="$PROJECT_ROOT/middleware/src"
+MIDDLEWARE_DIR="/middleware/src"
 MIDDLEWARE_VENV_DIR="$MIDDLEWARE_DIR/.venv"
 
 set -euo pipefail
@@ -9,11 +8,21 @@ log_step() {
 }
 export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
 
+if ! grep -qxF "$LINE_TO_ADD" "$BASHRC_FILE"; then
+    echo "Adding '$LINE_TO_ADD' to $BASHRC_FILE..."
+    echo "$LINE_TO_ADD" >> "$BASHRC_FILE"
+    echo "Line added successfully!"
+else
+    echo "The line is already present in $BASHRC_FILE. No changes made."
+fi
+
 log_step "Installing Python 3 and development headers..."
 sudo apt-get install -y python3
 
 log_step "Installing uv (Python package installer and manager)..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+source $HOME/.local/bin/env
 
 if command -v uv &> /dev/null; then
     log_step "uv installed successfully. Version: $(uv --version)"
@@ -37,7 +46,7 @@ log_step "Activating virtual environment and installing middleware dependencies.
 source "$MIDDLEWARE_VENV_DIR/bin/activate"
 
 log_step "Installing dependencies from middleware/pyproject.toml and uv.lock..."
-if uv sync; then
+if uv sync --project  "$MIDDLEWARE_VENV_DIR/pyproject.toml"; then
     log_step "Middleware dependencies installed successfully."
 else
     log_step "ERROR: Failed to install middleware dependencies using uv sync."
